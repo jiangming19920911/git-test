4100d1cb6a5197aa8f876e92c4933afcb577e02c
jiangming19920911 <jm19920911@163.com>
2017-11-25 20:31:14 +0800

    init

--- src/main/java/com/jm/springboot/TestController.java
+++ src/main/java/com/jm/springboot/TestController.java
@@ -1,9 +1,5 @@
 package com.jm.springboot;
 
-import com.jm.springboot.model.Product;
-import com.jm.springboot.service.SolrProductRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +12,6 @@ public class TestController {
 
     @RequestMapping("/index")
     public void index() {
-        SolrProductRepository solrProductRepository ;
     }
 
 }
